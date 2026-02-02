0e25d388309c4050ebc56a1624ee7d512ad46c3f
Nik <xniklasx.dev@gmail.com>
2026-02-02 13:18:23 +0100

    Change greeting from 'Hello TIMPLAN!' to 'Hello TIMPLAN Test!'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1> Hello TIMPLAN!</h1>
+        <h1> Hello TIMPLAN Test!</h1>
         <br/>
         <br/>
         <Image
