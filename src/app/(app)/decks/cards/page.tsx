9c6d04c522e187aeb4c40d2d2911e32819d9d2f8
Maximilian Krause <maximili.krause@gmail.com>
2026-02-11 21:09:07 +0100

    refactoring and globals.css changes

--- src/app/(app)/decks/cards/page.tsx
+++ src/app/(app)/decks/cards/page.tsx
@@ -1,10 +1,10 @@
 import styles from "./page.module.css";
 
-export default function Statistic() {
+export default function Cards() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1> Statistic Page</h1>
+        <h1> Cards Page</h1>
       </main>
     </div>
   );
