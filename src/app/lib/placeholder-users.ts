39456cbb380ca824667c8446e0bf24b43296a71b
Nik <xniklasx.dev@gmail.com>
2026-02-20 20:42:12 +0100

    deck:  added user placeholder + fixed deck page hover effect
    * removed unnecessary comments
    
    * added user placeholder + fixed deck page hover effect

---
+++ src/app/lib/placeholder-users.ts
@@ -0,0 +1,52 @@
+import { User } from "./definitions";
+
+const users: User[] = [
+    {
+        id: 1,
+        name: "Test User",
+        email: "test@example.com",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    { 
+        id: 2,
+        name: "Nik test",
+        email: "nik@example.com",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    {
+        id: 3,
+        name: "Max Test",
+        email: "max@example.com",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    { 
+        id: 4,
+        name: "Tony test",
+        email: "tony@example.com",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    { 
+        id: 5,
+        name: "Leonie test",
+        email: "leonie@example.com",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    },
+    { 
+        id: 6,
+        name: "Pascal test",
+        email: "pascal@example.com",
+        password: "password",
+        createdAt: new Date(),
+        updatedAt: new Date()
+    }
+]
