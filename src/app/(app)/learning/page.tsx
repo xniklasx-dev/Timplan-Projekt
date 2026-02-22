60c4d102b91c852799172e2871d9e49015bca8c0
Tony <136270183+mildaardvark4034@users.noreply.github.com>
2026-02-22 21:56:59 +0100

    Changes in learning and test data
    * Update question Layout and  skip, previous and hint button
    
    * Refactor learning module to use JSON insetad of ts testing data

--- src/app/(app)/learning/page.tsx
+++ src/app/(app)/learning/page.tsx
@@ -2,9 +2,37 @@
 import { useState } from "react";
 import styles from "./page.module.css";
 import LearnCard from "@/app/ui/learning_cards/learning_cards";
-import {decks} from "@/app/lib/placeholder-decks";
-import {cards} from "@/app/lib/placeholder-cards";
+
+import decksData from "@/app/lib/placeholder-decks.json";
+import cardsData from "@/app/lib/placeholder-cards.json";
+
+import { Deck, Card } from "@/app/lib/definitions";
 import { getCardsForDeck, rateCard } from "@/app/lib/learning-service";
+
+function hydrateCard(raw: any): Card {
+  return {
+    ...raw,
+    due: new Date(raw.due),
+    createdAt: new Date(raw.createdAt),
+    updatedAt: new Date(raw.updatedAt),
+    lastReview: raw.lastReview ? new Date(raw.lastReview) : undefined,
+  };
+}
+
+function hydrateDeck(raw: any): Deck {
+  return {
+    ...raw,
+    createdAt: new Date(raw.createdAt),
+    updatedAt: new Date(raw.updatedAt),
+    lastStudied: raw.lastStudied
+      ? new Date(raw.lastStudied)
+      : undefined,
+  };
+}
+
+const decks: Deck[] = decksData.map(hydrateDeck);
+const cards: Card[] = cardsData.map(hydrateCard);
+
 
 
 export default function Learning() {
@@ -16,7 +44,12 @@ export default function Learning() {
 
   const currentCard = deckCards[currentIndex];
 
-   const handleRate = (rating: 0 | 1 | 2 | 3) => {
+  const changeIndex = (index: number) => {
+    if (index >= 0 && index < deckCards.length) {
+      setCurrentIndex(index);
+    }};
+
+  const handleRate = (rating: 0 | 1 | 2 | 3) => {
     const updatedCard = rateCard(currentCard, rating);
 
     // Karte im Array ersetzen
@@ -32,7 +65,7 @@ export default function Learning() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>{selectedDeck.name} {currentIndex + 1}/{selectedDeck.totalCards}</h1>
-        <LearnCard card={currentCard} onRate={handleRate} />
+        <LearnCard card={currentCard} currentIndex={currentIndex} onRate={handleRate} changeIndex={changeIndex} />
       </main>
     </div>
   );
