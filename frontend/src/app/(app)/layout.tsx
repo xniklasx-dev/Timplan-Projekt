824a837650a9a82564c1547fc7f7b128d063b2d5
Max <153655826+lumenVe@users.noreply.github.com>
2026-02-23 18:25:28 +0100

    fix suspense error

--- frontend/src/app/(app)/layout.tsx
+++ frontend/src/app/(app)/layout.tsx
@@ -2,6 +2,7 @@ import Navbar from '../ui/navbar/Navbar';
 import styles from './layout.module.css';
 import placeholderUsers from "@/app/lib/placeholder-users.json";
 import { User } from '../lib/definitions';
+import { Suspense } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   const rawUser = placeholderUsers[0];
@@ -14,7 +15,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div className={styles.shell}>
-      <div className={styles.navbar}><Navbar user={user}/></div>
+      <Suspense fallback={null}>
+        <div className={styles.navbar}><Navbar user={user}/></div>
+      </Suspense>
       <main className={styles.main}>{children}</main>
     </div>
   );
