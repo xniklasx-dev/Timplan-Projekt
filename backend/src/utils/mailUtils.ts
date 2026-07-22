a7e71a460a6c9b3ba6064ebb968e5ac50d4a5555
Max Krause <153655826+lumenVe@users.noreply.github.com>
2026-07-23 00:15:40 +0200

    Update mailUtils to conditionally initialize Resend
    Refactor resend initialization to check for API key presence.

--- backend/src/utils/mailUtils.ts
+++ backend/src/utils/mailUtils.ts
@@ -2,9 +2,11 @@ import { Resend } from "resend";
 
 // TODO: fix later because of Resend API_KEY
 
-const resend = process.env.RESEND_API_KEY
+if(process.env.RESEND_API_KEY) {
+  const resend = process.env.RESEND_API_KEY
   ? new Resend(process.env.RESEND_API_KEY)
   : null;
+}
 
 export async function sendResetEmail(
   email: string,
